f021cd255f959fcf6e2dc85780ae928f4db140cc
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-09-14 23:19:27 -0400

    mobile phone version

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Footer(){
     return (
-        <p className="footer">created by Justin Hoang - devChallenges.io</p>
+        <p className="footer">© 2023 <span id = "author-name">Truong Hoang</span><i>All rights reserved.</i></p>
     )
 }
