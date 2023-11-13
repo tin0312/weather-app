f3b8fb122a7ab1928d77c8e8c87e4a8e36c8a456
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-11-13 13:32:04 -0500

    Migrate saved location to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,20 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-// import {getDatabase, ref, push, onValue, remove} from "firebase/database"
-// import { collection, addDoc } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyATbjB97MFqX7GQ3O85UGUF9IIBN4bhdVI",
-  authDomain: "weather-app-bcc26.firebaseapp.com",
-  projectId: "weather-app-bcc26",
-  storageBucket: "weather-app-bcc26.appspot.com",
-  messagingSenderId: "658888927124",
-  appId: "1:658888927124:web:e6a447487937ecbbd61181",
-  measurementId: "G-2ZYCVHTH7Y"
-};
-
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "./firebaseConfig";
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATbjB97MFqX7GQ3O85UGUF9IIBN4bhdVI",
+    authDomain: "weather-app-bcc26.firebaseapp.com",
+    projectId: "weather-app-bcc26",
+    storageBucket: "weather-app-bcc26.appspot.com",
+    messagingSenderId: "658888927124",
+    appId: "1:658888927124:web:e6a447487937ecbbd61181",
+    measurementId: "G-2ZYCVHTH7Y"
+  };
+
+export default firebaseConfig
